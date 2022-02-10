6f7d4be5599e5649b4fefeed478377dafb92743f
lenti821348788 <821348788@qq.com>
2022-02-10 15:34:17 +0800

    review

--- medium/300. 最长递增子序列.js
+++ medium/300. 最长递增子序列.js
@@ -37,37 +37,106 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-	let tails = Array(nums.length).fill(0);
-	let res = 0;
-	for (let key in nums) {
-		let left = 0,
-			right = res;
-		while (left < right) {
-			let mid = (left + right) >> 1;
-			if (tails[mid] < nums[key]) {
-				left = mid + 1;
-			} else {
-				right = mid;
-			}
-		}
-		tails[left] = nums[key];
-		if (right === res) {
-			res += 1;
-		}
-	}
-	return res;
+    let tails = Array(nums.length).fill(0);
+    let res = 0;
+    for (let key in nums) {
+        let left = 0,
+            right = res;
+        while (left < right) {
+            let mid = (left + right) >> 1;
+            if (tails[mid] < nums[key]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        tails[left] = nums[key];
+        if (right === res) {
+            res += 1;
+        }
+    }
+    return res;
 };
 
 var lengthOfLIS = function (nums) {
-	let len = nums.length;
-	if (!len) return 0;
-	let dep = Array(len).fill(1);
-	for (let i in nums) {
-		for (let j = 0; j < i; j++) {
-			if (nums[i] > nums[j]) {
-				dep[i] = Math.max(dep[i], dep[j] + 1);
-			}
-		}
-	}
-	return Math.max(...dep);
+    let len = nums.length;
+    if (!len) return 0;
+    let dep = Array(len).fill(1);
+    for (let i in nums) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dep[i] = Math.max(dep[i], dep[j] + 1);
+            }
+        }
+    }
+    return Math.max(...dep);
 };
+
+// // VUE3 实现
+var lengthOfLIS = function (arr) {
+    // p 里面的内容无所谓, 记录的有效值是索引,当前下标的值是上一个值的索引
+    const p = arr.slice();
+    // result存储的是结果的索引, 默认放入第一项
+    const result = [0];
+    let i, j, u, v, c;
+    // 数组长度
+    const len = arr.length;
+    for (i = 0; i < len; i++) {
+        // 第i项
+        const arrI = arr[i];
+        //  第i项的值不能是0
+        if (arrI !== 0) {
+            // j是结果索引数组的最后一项
+            j = result[result.length - 1];
+            // 第i项比结果数组中的最后一项大,存入结果
+            if (arr[j] < arrI) {
+                // 记录i位置对应的前一项的索引
+                p[i] = j;
+                // 结果索引数组放入i
+                result.push(i);
+                // 下一轮循环
+                continue;
+            }
+            // 如果第i项小于j,进行下面操作
+            // u,v为结果数组的起始点和结尾点
+            u = 0;
+            v = result.length - 1;
+            // 循环 起始点小于结尾点
+            while (u < v) {
+                // c是中点, 二分查找
+                c = (u + v) >> 1;
+                // arrI大于中点, 起始点右移,赋值中值+1
+                if (arr[result[c]] < arrI) {
+                    u = c + 1;
+                } else {
+                    // 结尾点左移,赋值中值
+                    v = c;
+                }
+            }
+            // 第i值 依旧小于目标中值, 相同或者更大就不需要更换
+            if (arrI < arr[result[u]]) {
+                // 起点大于0
+                if (u > 0) {
+                    // 将它替换的前一项记住
+                    p[i] = result[u - 1];
+                }
+                // 更新目标值下标结果 为 i
+                result[u] = i;
+            }
+        }
+    }
+    // u是结果数组长度, v是结果最后一项
+    u = result.length;
+    v = result[u - 1];
+    // 回溯数组p, 依次找到最终的索引
+    while (u-- > 0) {
+        // 重新组织结果
+        result[u] = v;
+        v = p[v];
+    }
+    // return result;
+    // return result.map((item, index) => arr[item]);
+    return result.length;
+};
+
+console.log('ans', lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
