7685000bc4b28b7723384fc6ad320ef9490cd897
lenti821348788 <821348788@qq.com>
2022-03-30 18:36:06 +0800

    feat

---
+++ easy/338. 比特位计数.js
@@ -0,0 +1,100 @@
+/**
+ * 
+ * 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+
+ 
+
+示例 1：
+
+输入：n = 2
+输出：[0,1,1]
+解释：
+0 --> 0
+1 --> 1
+2 --> 10
+示例 2：
+
+输入：n = 5
+输出：[0,1,1,2,1,2]
+解释：
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+提示：
+
+0 <= n <= 105
+ 
+
+进阶：
+
+很容易就能实现时间复杂度为 O(n log n) 的解决方案，你可以在线性时间复杂度 O(n) 内用一趟扫描解决此问题吗？
+你能不使用任何内置函数解决此问题吗？（如，C++ 中的 __builtin_popcount ）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/counting-bits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let ans = []
+    for( let i = 0; i <= n; i++ ) {
+        let t = 0;
+        let v = i;
+        do {
+            if(v & 1) {
+                t++
+            }
+        }while(v >>= 1)
+        ans.push(t)
+    }
+    return ans;
+};
+
+var countBits = function(n) {
+    const bits = new Array(n + 1).fill(0);
+    for (let i = 0; i <= n; i++) {
+        bits[i] = countOnes(i);
+    }
+    return bits
+};
+
+// Brian Kernighan 算法, x &= (x - 1)该运算将 x 的二进制表示的最后一个 1 变成 0。因此，对 x 重复该操作，直到 x 变成 0，则操作次数即为 x 的一比特数
+const countOnes = (x) => {
+    let ones = 0;
+    while (x > 0) {
+        x &= (x - 1);
+        ones++;
+    }
+    return ones;
+}
+
+// 最高有效位
+var countBits = function(n) {
+    const bits = new Array(n + 1).fill(0);
+    let highBit = 0;
+    for (let i = 1; i <= n; i++) {
+        if ((i & (i - 1)) == 0) {
+            highBit = i;
+        }
+        bits[i] = bits[i - highBit] + 1;
+    }
+    return bits;
+};
+
+// 最低有效位
+var countBits = function(n) {
+    const bits = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        bits[i] = bits[i >> 1] + (i & 1);
+    }
+    return bits;
+};
